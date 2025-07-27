806316b60dc13476046e6eee22d836413a8e362d
saravanacode <saravanaperumalnataraj9@gmail.com>
2025-07-27 11:58:37 +0530

    addd change flask

--- frontend/project/next.config.js
+++ frontend/project/next.config.js
@@ -14,6 +14,19 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000',
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests; connect-src 'self' http://167.86.83.164:5000 https://167.86.83.164:5000 http://api.pmshaver.pro http://localhost:5000 https://localhost:5000;"
+          },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
